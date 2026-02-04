3ea8fb306ac13bfd8870590b4b6eafe1353df229
rpp-min1ciamis <rpp.min1ciamis@gmail.com>
2026-02-05 00:33:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Generator RPP KBC MI",
+    layout="centered"
+)
+
+st.title("📘 Generator RPP Kurikulum Berbasis Cinta")
+st.subheader("MI Negeri 1 Ciamis")
+
+st.success("Aplikasi berhasil dijalankan 🎉")
+
+st.write("""
+Versi:
+- Kemenag
+- Terintegrasi KBC (Cinta, Adab, Nilai)
+- Online & bisa diakses guru lain
+""")
